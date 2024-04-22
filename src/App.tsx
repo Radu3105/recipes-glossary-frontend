333c73ca24f9b81f1be512a78eff37a07c193a4a
Radu3105 <radu.girlea@yahoo.com>
2024-04-22 21:15:43 +0300

    add published-recipes-by-author modal

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ import Modal from "./components/Modal";
 import { sToTime } from "./helpers";
 
 interface Recipe {
-    recipeID: string;
+    recipeId: string;
     recipeName: string;
     authorName: string;
     ingredientCount: number;
     skillLevel: string;
+}
+
+interface RecipeByAuthor {
+    recipeId: string;
+    recipeName: string;
 }
 
 interface RecipeDetails {
@@ -23,20 +28,24 @@ interface RecipeDetails {
     cookingTime: number;
     preparationTime: number;
     ingredients: string[];
+    collections: string[];
+    keywords: string[];
+    dietTypes: string[];
 }
 
 const App: React.FC = () => {
     const RECIPES_PER_PAGE: number = 20;
-
+    
     const [recipeData, setRecipeData] = useState<Recipe[]>([]);
-    const [recipeDetailsData, setRecipeDetailsData] =
-        useState<RecipeDetails | null>(null);
-    const [selectedRecipeData, setSelectedRecipeData] = useState<Recipe | null>(
-        null
-    );
+    const [recipesByAuthorData, setRecipesByAuthorData] = useState<RecipeByAuthor[]>([]);
+    const [recipeDetailsData, setRecipeDetailsData] = useState<RecipeDetails | null>(null);
+    const [selectedAuthor, setSelectedAuthor] = useState<string | null>("");
     const [currentPage, setCurrentPage] = useState<number>(1);
+    const [authorModalCurrentPage, setAuthorModalCurrentPage] =useState<number>(1);
     const [totalPages, setTotalPages] = useState<number>(0);
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [totalPagesByAuthor, setTotalPagesByAuthor] = useState<number>(0);
+    const [isRecipeModalOpen, setIsRecipeModalOpen] = useState<boolean>(false);
+    const [isAuthorModalOpen, setIsAuthorModalOpen] = useState<boolean>(false);
 
     // Fetch the total number of recipes when the application starts.
     useEffect(() => {
@@ -68,11 +77,31 @@ const App: React.FC = () => {
         fetchRecipes();
     }, [currentPage]);
 
+    useEffect(() => {
+        if (selectedAuthor) {
+            fetchRecipesByAuthor(selectedAuthor);
+        }
+    }, [authorModalCurrentPage, selectedAuthor]);
+
+    // Fetch the total number of recipes for a specific author.
+    const fetchTotalPagesByAuthor = async (
+        authorName: string
+    ): Promise<void> => {
+        try {
+            const response = await axios.get<number>(
+                `https://localhost:44389/Recipes/count/${authorName}`
+            );
+            setTotalPagesByAuthor(Math.ceil(response.data / RECIPES_PER_PAGE));
+        } catch (error) {
+            console.error("Error " + error);
+        }
+    };
+
     // Fetch the details of a specific recipe by a recipe id
     const fetchRecipeDetails = async (recipeId: string): Promise<void> => {
         try {
             const response = await axios.get<RecipeDetails>(
-                `https://localhost:44389/Recipes/${recipeId}`
+                `https://localhost:44389/Recipes/id/${recipeId}`
             );
             setRecipeDetailsData(response.data);
         } catch (error) {
@@ -80,14 +109,80 @@ const App: React.FC = () => {
         }
     };
 
+    // Fetch the recipes by a specific author
+    const fetchRecipesByAuthor = async (authorName: string): Promise<void> => {
+        try {
+            const response = await axios.get<RecipeByAuthor[]>(
+                `https://localhost:44389/Recipes/${authorName}/${authorModalCurrentPage}`
+            );
+            setRecipesByAuthorData(response.data);
+        } catch (error) {
+            console.error("Error " + error);
+        }
+    };
+
+    const handleOnRecipeClick = async (recipe: Recipe): Promise<void> => {
+        setIsRecipeModalOpen(true);
+        setIsAuthorModalOpen(false);
+        await fetchRecipeDetails(recipe.recipeId);
+    };
+
+    const handleOnAuthorRecipeClick = async (authorRecipeId: string): Promise<void> => {
+        setIsRecipeModalOpen(true);
+        setIsAuthorModalOpen(false);
+        await fetchRecipeDetails(authorRecipeId);
+    };
+
+    const handleOnAuthorClick = async (
+        event: React.ChangeEvent<unknown>,
+        authorName: string
+    ): Promise<void> => {
+        event.stopPropagation();
+        setIsAuthorModalOpen(true);
+        setIsRecipeModalOpen(false);
+        setAuthorModalCurrentPage(1);
+        setSelectedAuthor(authorName);
+        await fetchTotalPagesByAuthor(authorName);
+        await fetchRecipesByAuthor(authorName);
+    };
+
+    const handlePageChange = (
+        event: React.ChangeEvent<unknown>,
+        value: number
+    ): void => {
+        setCurrentPage(value);
+    };
+
+    const handleAuthorModalPageChange = (
+        event: React.ChangeEvent<unknown>,
+        value: number
+    ): void => {
+        setAuthorModalCurrentPage(value);
+    };
+
+    const handleCloseModal = async () => {
+        setIsAuthorModalOpen(false);
+        setAuthorModalCurrentPage(1);
+        if (selectedAuthor) {
+            await fetchRecipesByAuthor(selectedAuthor);
+        }
+    };
+
     const renderRecipes = (): ReactElement[] => {
         return recipeData.map((recipe: Recipe) => (
             <tr
-                key={recipe.recipeID}
+                key={recipe.recipeId}
                 onClick={() => handleOnRecipeClick(recipe)}
             >
                 <td>{recipe.recipeName}</td>
-                <td>{recipe.authorName}</td>
+                <td
+                    className="recipe-table-author"
+                    onClick={(event) =>
+                        handleOnAuthorClick(event, recipe.authorName)
+                    }
+                >
+                    {recipe.authorName}
+                </td>
                 <td>{recipe.ingredientCount}</td>
                 <td
                     className={"skill-" + recipe.skillLevel.split(" ").join("")}
@@ -99,17 +194,14 @@ const App: React.FC = () => {
     };
 
     const renderRecipeDetailsModal = (): ReactElement | undefined => {
-        if (isModalOpen) {
+        if (isRecipeModalOpen) {
             return (
-                <Modal onClose={() => setIsModalOpen(false)}>
-                    {recipeDetailsData && selectedRecipeData && (
+                <Modal onClose={() => setIsRecipeModalOpen(false)}>
+                    {recipeDetailsData && (
                         <>
                             <h1 className="modal-title">
-                                {selectedRecipeData.recipeName}
+                                {recipeDetailsData.name}
                             </h1>
-                            <p className="modal-author">
-                                by {selectedRecipeData.authorName}
-                            </p>
                             <p>
                                 Preparation time:{" "}
                                 <b>
@@ -128,26 +220,12 @@ const App: React.FC = () => {
                                     </span>
                                 </b>
                             </p>
-                            <p>
-                                Skill level:{" "}
-                                <span
-                                    className={
-                                        "skill-" +
-                                        selectedRecipeData.skillLevel
-                                            .split(" ")
-                                            .join("")
-                                    }
-                                >
-                                    {selectedRecipeData.skillLevel}
-                                </span>
-                            </p>
                             <div className="modal-content-group">
                                 <div className="modal-group-1">
                                     <h2>Description</h2>
                                     <p>{recipeDetailsData.description}</p>
                                     <h2>
-                                        Ingredients (
-                                        {selectedRecipeData.ingredientCount})
+                                        Ingredients
                                     </h2>
                                     <ul>
                                         {recipeDetailsData.ingredients.map(
@@ -164,6 +242,7 @@ const App: React.FC = () => {
                                         )}
                                     </ul>
                                 </div>
+                                <div className="modal-group-2"></div>
                             </div>
                         </>
                     )}
@@ -172,22 +251,46 @@ const App: React.FC = () => {
         }
     };
 
-    const handleOnRecipeClick = (recipe: Recipe): void => {
-        setIsModalOpen(true);
-        fetchRecipeDetails(recipe.recipeID);
-        setSelectedRecipeData(recipe);
-    };
-
-    const handlePageChange = (
-        event: React.ChangeEvent<unknown>,
-        value: number
-    ): void => {
-        setCurrentPage(value);
+    const renderAuthorDetailsModal = (): ReactElement | undefined => {
+        if (isAuthorModalOpen) {
+            return (
+                <Modal onClose={handleCloseModal}>
+                    {selectedAuthor && (
+                        <>
+                            <h1 className="author-modal-title">{selectedAuthor}</h1>
+                            <table className="author-table">
+                                <tbody>
+                                    {recipesByAuthorData.map(
+                                        (recipeByAuthor: RecipeByAuthor) => (
+                                            <tr onClick={() => handleOnAuthorRecipeClick(recipeByAuthor.recipeId)}>{recipeByAuthor.recipeName}</tr>
+                                        )
+                                    )}
+                                </tbody>
+                            </table>
+                            <div className="author-pagination-group">
+                                <div className="pagination">
+                                    <Stack spacing={2}>
+                                        <Pagination
+                                            count={totalPagesByAuthor}
+                                            page={authorModalCurrentPage}
+                                            onChange={
+                                                handleAuthorModalPageChange
+                                            }
+                                        />
+                                    </Stack>
+                                </div>
+                            </div>
+                        </>
+                    )}
+                </Modal>
+            );
+        }
     };
 
     return (
         <div className="app-container">
             {renderRecipeDetailsModal()}
+            {renderAuthorDetailsModal()}
             <div className="app-title">
                 <h1>Recipes Glossary</h1>
                 <p>
